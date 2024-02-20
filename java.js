8bf71bbbf0ec9993006a9dce47848d33747604e7
Nadi2894 <nadirabegimnadyrbekova@gmail.com>
2024-02-20 17:30:02 +0100

    added AI and Axios

--- java.js
+++ java.js
@@ -1,7 +1,22 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
-  let poemElement = document.querySelector("#poem");
-  poemElement.innerHTML = "The poem will go header";
+  let userinstructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "a41b6cct8f269ddeo4da44bf9300d179";
+  let context =
+    " Your mission is to write Hungarian poem in seperated lines with <br/>.Do not include the title of the poem. Include the author of the poem";
+  let prompt = `Please generate Hungarian poem about${userinstructionsInput.value}`;
+  let ApiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(ApiUrl).then(displayPoem);
 }
 
 let poemGeneratorElement = document.querySelector("#poemGenerator");
